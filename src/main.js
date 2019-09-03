eca4ba053746769facff79ab5411edf8546e1b66
Student <example@example.com>
2019-09-03 16:07:50 -0700

    Fallow along with the lesson to learn webpack basics.

---
+++ src/main.js
@@ -0,0 +1,16 @@
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import $ from 'jquery';
+import './styles.css';
+import { pingPong } from './ping-pong';
+
+$(document).ready(function() {
+  $('#ping-pong-form').submit(function(event) {
+    event.preventDefault();
+    var goal = $('#goal').val();
+    var output = pingPong(goal);
+    output.forEach(function(element) {
+      $('#solution').append("<li>" + element + "</li>");
+    });
+  });
+});
